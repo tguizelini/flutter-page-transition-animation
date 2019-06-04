2f576a0c76fb4ed9a223ceda53aa7339ef8cf8b0
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-06-04 17:50:54 -0300

    pages Home and OtherPage

---
+++ lib/pages/other_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OtherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Other Page")),
+      body: Center(
+        child: Text("My Page")
+      ),
+    );
+  }
+}
