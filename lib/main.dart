ad5bbf420ce306cc1664029b94a0791542e2082d
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-06-04 17:58:01 -0300

    buttons at HomePage to make transitions test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_page_transition_animation/pages/home.dart';
 
-void main() => runApp(HomePage());
+void main() => runApp(
+  MaterialApp(
+    home: HomePage()
+  )
+);
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_page_transition_animation/nav_transition/nav_slide_from_bottom.dart';
+import 'package:flutter_page_transition_animation/nav_transition/nav_slide_from_left.dart';
+import 'package:flutter_page_transition_animation/nav_transition/nav_slide_from_right.dart';
+import 'package:flutter_page_transition_animation/nav_transition/nav_slide_from_top.dart';
+
+import 'other_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -7,9 +13,30 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Column(children: <Widget>[
-
-      ]),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () => NavSlideFromBottom(page: OtherPage()),
+              child: Text("From Bottom")
+            ),
+            RaisedButton(
+              onPressed: () => NavSlideFromTop(page: OtherPage()),
+              child: Text("From Top")
+            ),
+            RaisedButton(
+              onPressed: () => NavSlideFromLeft(page: OtherPage()),
+              child: Text("From Left")
+            ),
+            RaisedButton(
+              onPressed: () => NavSlideFromRight(page: OtherPage()),
+              child: Text("From Right")
+            )
+          ]
+        ),
+      )
     );
   }
 }
